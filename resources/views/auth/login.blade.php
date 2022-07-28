aeb0afc743e1145d6fdf5a7bf02d021883dbe543
Napollo LLC <nouman@napollo.net>
2022-07-28 04:30:25 -0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Login | Intrustpit</title>
+    <title>Logins | Intrustpit</title>
 
     <meta name="description" content="" />
 
